3ebfee55f07a5120f9908db1c51d5b9517364d98
Edith Mandujano <edithmandujano001@gmail.com>
2017-04-10 10:33:45 -0500

    Tarjetas 2

--- ejercicio-01/ejercicio-01.js
+++ ejercicio-01/ejercicio-01.js
@@ -1,7 +1,17 @@
 function isValidCard(creditCardNumber) {
     // Escribe tu código aquí
-    
+    if (creditCardNumber!==Number){
+      return("Ingresa un numero");
+      return("Error de dato");
+    }
+    if (creditCardNumber.length<16){
+      return("Faltan numeros");
+    }
+
+
 }
+
+
 
 // NO TOCAR ESTE CÓDIGO O EXPLOTARÁ LA PC EN 10 SEGUNDOS
 if (typeof exports !== 'undefined') {
